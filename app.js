9c77231eca8a635f5daa5aaf5333a11b9a9e26d8
iamUxer <bonethecomer@gmail.com>
2019-01-08 15:56:34 +0900

    counter-app.finished

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 let counter = 0;
 
-counter.__proto__.increment = function(){
-    counter = counter + 1;
+counter.__proto__.increment = function(num){
+    if (num){
+        counter = counter + num;
+    } else {
+        counter = counter + 1;
+    }
+    return counter;
+}
+
+counter.__proto__.decrement = function(num){
+    if(num){
+        counter = counter - num;
+    } else {
+        counter = counter - 1;
+    }
+    return counter;
+}
+
+counter.__proto__.reset = function(){
+    counter = 0;
     return counter;
 }
 
 console.log(counter.increment());
-console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.decrement(3));
+console.log(counter.increment(4));
+console.log(counter.reset());
